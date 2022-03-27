aa5881d628c8019bc0393b7f2ba30a2c43ff74d3
rrudolfs <102427411+rrudolfs@users.noreply.github.com>
2022-03-27 14:13:47 +0200

    Vizuāls 1.0

---
+++ kontroldarbs_Desas/kontroldarbs_Desas/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace kontroldarbs_Desas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelVirsraksts = new System.Windows.Forms.Label();
+            this.SaktPoga = new System.Windows.Forms.Button();
+            this.InformacijaPoga = new System.Windows.Forms.Button();
+            this.AizvertPoga = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelVirsraksts
+            // 
+            this.labelVirsraksts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelVirsraksts.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.labelVirsraksts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelVirsraksts.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelVirsraksts.Location = new System.Drawing.Point(92, 89);
+            this.labelVirsraksts.Name = "labelVirsraksts";
+            this.labelVirsraksts.Size = new System.Drawing.Size(394, 182);
+            this.labelVirsraksts.TabIndex = 0;
+            this.labelVirsraksts.Text = "Spēle \"Desas\"";
+            this.labelVirsraksts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SaktPoga
+            // 
+            this.SaktPoga.BackColor = System.Drawing.Color.YellowGreen;
+            this.SaktPoga.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.SaktPoga.Location = new System.Drawing.Point(197, 359);
+            this.SaktPoga.Name = "SaktPoga";
+            this.SaktPoga.Size = new System.Drawing.Size(166, 123);
+            this.SaktPoga.TabIndex = 1;
+            this.SaktPoga.Text = "Sākt";
+            this.SaktPoga.UseVisualStyleBackColor = false;
+            // 
+            // InformacijaPoga
+            // 
+            this.InformacijaPoga.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.InformacijaPoga.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.InformacijaPoga.Location = new System.Drawing.Point(424, 411);
+            this.InformacijaPoga.Name = "InformacijaPoga";
+            this.InformacijaPoga.Size = new System.Drawing.Size(111, 71);
+            this.InformacijaPoga.TabIndex = 2;
+            this.InformacijaPoga.Text = "Info";
+            this.InformacijaPoga.UseVisualStyleBackColor = false;
+            // 
+            // AizvertPoga
+            // 
+            this.AizvertPoga.BackColor = System.Drawing.Color.Brown;
+            this.AizvertPoga.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.AizvertPoga.Location = new System.Drawing.Point(25, 411);
+            this.AizvertPoga.Name = "AizvertPoga";
+            this.AizvertPoga.Size = new System.Drawing.Size(111, 71);
+            this.AizvertPoga.TabIndex = 3;
+            this.AizvertPoga.Text = "Aizvērt";
+            this.AizvertPoga.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(581, 635);
+            this.Controls.Add(this.AizvertPoga);
+            this.Controls.Add(this.InformacijaPoga);
+            this.Controls.Add(this.SaktPoga);
+            this.Controls.Add(this.labelVirsraksts);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Desas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVirsraksts;
+        private System.Windows.Forms.Button SaktPoga;
+        private System.Windows.Forms.Button InformacijaPoga;
+        private System.Windows.Forms.Button AizvertPoga;
+    }
+}
+
---
+++ kontroldarbs_Desas/kontroldarbs_Desas/Form2.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace kontroldarbs_Desas
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelVirsraksts = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.AizvertPoga = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelVirsraksts
+            // 
+            this.labelVirsraksts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelVirsraksts.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.labelVirsraksts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelVirsraksts.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelVirsraksts.Location = new System.Drawing.Point(158, 19);
+            this.labelVirsraksts.Name = "labelVirsraksts";
+            this.labelVirsraksts.Size = new System.Drawing.Size(253, 69);
+            this.labelVirsraksts.TabIndex = 1;
+            this.labelVirsraksts.Text = "Spēle \"Desas\"";
+            this.labelVirsraksts.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 135);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 135);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(364, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 135);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(364, 264);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 135);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(216, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 135);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(68, 264);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 135);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(364, 405);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 135);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(216, 405);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(142, 135);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(68, 405);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(142, 135);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // AizvertPoga
+            // 
+            this.AizvertPoga.BackColor = System.Drawing.Color.Brown;
+            this.AizvertPoga.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.AizvertPoga.Location = new System.Drawing.Point(484, 573);
+            this.AizvertPoga.Name = "AizvertPoga";
+            this.AizvertPoga.Size = new System.Drawing.Size(85, 50);
+            this.AizvertPoga.TabIndex = 11;
+            this.AizvertPoga.Text = "Aizvērt";
+            this.AizvertPoga.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Chocolate;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button10.Location = new System.Drawing.Point(12, 573);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(85, 50);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Restart";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(581, 635);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.AizvertPoga);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelVirsraksts);
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.ShowIcon = false;
+            this.Text = "Desas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelVirsraksts;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button AizvertPoga;
+        private System.Windows.Forms.Button button10;
+    }
+}
